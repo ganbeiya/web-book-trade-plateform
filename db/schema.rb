2a0e2173d845a97e1fe41ae64e8880df9b823c10
ganbeiya04 <yang.6390@buckeyemail.osu.edu>
2025-09-03 18:35:13 -0400

    setup on the mac

--- db/schema.rb
+++ db/schema.rb
@@ -117,9 +117,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_27_171802) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "email_notifications", default: true
     t.string "username"
     t.text "introduction"
-    t.boolean "email_notifications", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
